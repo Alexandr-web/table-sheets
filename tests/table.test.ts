6731e8b40405a577bc46b5f7b796355c095114df
Alexandr-web <alexandr.yakovlev228@gmail.com>
2025-06-29 19:28:36 +0300

    test(bundler): добавил тесты для table и pxToVw

---
+++ tests/table.test.ts
@@ -0,0 +1,89 @@
+import { expect, test } from "@jest/globals";
+
+import Table from "../src/classes/Table";
+import { ICell, ITable } from "../src/interfaces";
+import { TLettersArr } from "../src/types";
+import { EnglishAlphabet, Colors } from "../src/enums";
+
+describe("Тест класса Table", () => {
+    test("создание экземпляра класса", () => {
+        const table: ITable = new Table();
+
+        expect(table).toBeTruthy();
+    });
+
+    test("проверка количества чисел (передано 123)", () => {
+        const table: ITable = new Table(123);
+
+        expect(table._countNums).toBe(123);
+    });
+
+    test("проверка количества чисел (передано 0)", () => {
+        const table: ITable = new Table(26);
+
+        expect(table._countNums).toBe(26);
+    });
+
+    test("проверка количества чисел (передано 1000)", () => {
+        const table: ITable = new Table(1000);
+
+        expect(table._countNums).toBe(702);
+    });
+
+    test("проверка длины английского алфавита", () => {
+        const table: ITable = new Table();
+
+        expect(table._getLengthEnglishAlphabet()).toBe(26);
+    });
+
+    test("проверка заполнения чисел таблицы (передано 26 чисел)", () => {
+        const table: ITable = new Table();
+
+        expect(table._fillNums().length).toBe(26);
+    });
+
+    test("проверка заполнения чисел таблицы (передано 45 чисел)", () => {
+        const table: ITable = new Table(45);
+
+        expect(table._fillNums().length).toBe(45);
+    });
+
+    test("проверка заполнения букв таблицы (передано 26 букв)", () => {
+        const table: ITable = new Table();
+        const res: TLettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+        expect(JSON.stringify(table._fillLetters())).toBe(JSON.stringify(res));
+    });
+
+    test("проверка заполнения букв таблицы (передано 30 букв)", () => {
+        const table: ITable = new Table(30);
+        const res: TLettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD"];
+
+        expect(JSON.stringify(table._fillLetters())).toBe(JSON.stringify(res));
+    });
+
+    test("проверка заполнения ячеек таблицы (передано 26 чисел)", () => {
+        const table: ITable = new Table();
+        const res1: ICell = {
+            position: [EnglishAlphabet[0], 1],
+            content: "",
+            color: Colors.BLACK,
+            background: Colors.WHITE,
+        };
+
+        const res2: ICell = {
+            position: [EnglishAlphabet[0], 26],
+            content: "",
+            color: Colors.BLACK,
+            background: Colors.WHITE,
+        };
+
+        table.letters = table._fillLetters();
+        table.nums = table._fillNums();
+
+        const cells: Array<ICell> = table._fillCells();
+
+        expect(JSON.stringify(cells[0])).toBe(JSON.stringify(res1));
+        expect(JSON.stringify(cells[25])).toBe(JSON.stringify(res2));
+    });
+});
